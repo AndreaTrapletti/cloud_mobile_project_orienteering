b356867bc8b62709681d4a5cf6c237c7129004f4
Andrea Trapletti <67322071+AndreaTrapletti@users.noreply.github.com>
2022-05-08 19:34:17 +0200

    Create GETfunction.py

---
+++ homework2/GET/GETfunction.py
@@ -0,0 +1,132 @@
+import json, boto3, botocore.exceptions
+from xml.dom import minidom
+from lib import func
+from boto3.dynamodb.conditions import Key
+f1 = func.func()
+bucket_name = "garetgv"
+def lambda_handler(event, context):
+    
+    global bucket_name
+    path = event["path"].split("/")
+    path = path[len(path)-1]
+    
+    if path == "list_races":
+        sess= boto3.Session(region_name='us-east-1')
+        ddb= sess.client('dynamodb')
+        table = "TokenTable"
+        TableItems = ddb.scan(TableName = table)["Items"]
+        item ={}
+        for x in range(len(TableItems)): 
+            evento = "evento"+ str(x+1)
+            gara = {
+                
+                "race_name" : TableItems[x]["raceName"]["S"],
+                "race_date" : TableItems[x]["raceDate"]["S"],
+                "race_id"   : TableItems[x]["race_id"]["S"]
+            }
+            item[evento]=gara
+        return {
+            'statusCode': 200,
+            'body': json.dumps(item)
+        }
+    elif path.split("?")[0]=="list_classes":
+        sess= boto3.Session(region_name='us-east-1')
+        ddb= sess.client('dynamodb')
+        table = "GareOrienteering"
+        id = event["queryStringParameters"]["id"]
+        TableItems = ddb.scan(TableName = table)["Items"]
+        
+        item ={}
+        for x in range(len(TableItems)):
+            
+            if id == TableItems[x]["race_id"]["S"]:
+                evento = "evento"+ str(x+1)
+                gara = {
+                        
+                    "race_id"   : id,
+                    "categoria" : TableItems[x]["Categoria"]["S"]
+                }
+                item[evento]=gara
+    
+        return {
+            'statusCode': 200,
+            'body': json.dumps(item)
+        }
+                
+            
+    elif path.split("?")[0]=="results":
+        
+        if len(event["queryStringParameters"]) == 2:
+            filename = event["queryStringParameters"]["id"]+event["queryStringParameters"]["Categoria"]+".xml"
+            s3 = boto3.resource("s3")
+            
+            try:
+                s3_client = boto3.client("s3")
+                file = s3_client.get_object(Bucket=bucket_name, Key=filename)["Body"].read()
+                
+                xmldoc = minidom.parseString(file)
+                result = f1.simulator(xmldoc)
+                
+            except botocore.exceptions.ClientError as error:
+                if error.response['Error']['Code']=="NoSuchKey":
+                    return {
+                        'statusCode': 403,
+                        'body': "id o classe non trovato"
+                    }
+                else: 
+                    return {
+                        'statusCode': 400,
+                        'body': "errore non specificato"
+                    }
+                        
+            return {
+                'statusCode': 200,
+                'body': json.dumps(result)
+            }
+        else:
+            
+            organizzazione = event["queryStringParameters"]["organization"]
+            
+            filename = event["queryStringParameters"]["id"]+event["queryStringParameters"]["Categoria"]+".xml"
+            s3 = boto3.resource("s3")
+        
+            s3_client = boto3.client("s3")
+            file = s3_client.get_object(Bucket=bucket_name, Key=filename)["Body"].read()
+            
+            xmldoc = minidom.parseString(file)
+            lista  = xmldoc.getElementsByTagName("TeamResult")
+            atleti = []
+            for x in range(len(lista)):
+                   
+                N_organizzazione = lista[x].getElementsByTagName("Organisation")
+               
+                if len(N_organizzazione) == 1:
+                    if lista[x].getElementsByTagName('Name')[1].firstChild.data == organizzazione:
+
+                        for y in range(len(lista[x].getElementsByTagName("Person"))):
+                            atleta = str(lista[x].getElementsByTagName("Person")[y].getElementsByTagName("Family")[0].firstChild.data) + " " + str(lista[x].getElementsByTagName("Person")[y].getElementsByTagName("Given")[0].firstChild.data)
+                            atleti.append(atleta)
+                else:
+                    membri = lista[x].getElementsByTagName("TeamMemberResult")
+                    a = len(N_organizzazione) - len(membri)
+                    for k in range(a, len(N_organizzazione)):
+                        if N_organizzazione[k].getElementsByTagName("Name")[0].firstChild.data == organizzazione: 
+                            atleta = str(membri[k-a].getElementsByTagName("Family")[0].firstChild.data) + " " + str(membri[k-a].getElementsByTagName("Given")[0].firstChild.data)
+                            atleti.append(atleta)
+                    
+            atleti = sorted(atleti, key=lambda x:(x[0]))        
+            return {
+                'statusCode': 200,
+                'body': json.dumps(atleti)
+            }
+    
+    else :
+       return { 
+            'statusCode': 300,
+            'body': json.dumps("ERRORE: Ricontrollare endpoint inserito")
+                 }
+
+
+
+
+
