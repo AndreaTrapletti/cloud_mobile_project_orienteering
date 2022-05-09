7b27837dc4e7573d23fec25f7005c1baa2e49147
Mattia Giudici <67323575+Mattia-Giudici@users.noreply.github.com>
2022-05-09 09:39:21 +0200

    controlli aggiuntivi sul codice gara

--- homework2/register_race/register_race.py
+++ homework2/register_race/register_race.py
@@ -1,19 +1,30 @@
-
 import json,boto3
 from random import randint
-
+correttore = 0
 
 def lambda_handler(event, context):
-    uniquecode = uniquecodegen()
+    global correttore
+    sess= boto3.Session(region_name='us-east-1')
+    ddb= sess.client('dynamodb')
+    table = 'TokenTable'
     nome_gara = event["queryStringParameters"]["race_name"]
     date = event["queryStringParameters"]["race_date"]
     email = event["queryStringParameters"]["email"]
     date = date.split("/")
-    id = nome_gara[0]+nome_gara[1] + nome_gara[2]+ date[1]+date[2]
+    uniquecode = uniquecodegen()
     token = nome_gara+'_'+uniquecode
-    sess= boto3.Session(region_name='us-east-1')
-    ddb= sess.client('dynamodb')
-    table = 'TokenTable'
+    id = nome_gara[0]+nome_gara[1] + nome_gara[2]+ date[1]+date[2]
+    try:
+        ddb.get_item(TableName=table, Key={
+                'race_id': {
+                  'S': id
+                        }
+                    },
+                    )
+        id = id+str(correttore)
+        correttore = correttore+1
+    except KeyError :
+        print("race id non in conflitto")
     item = {
           "race_id": {
                 "S": id
@@ -42,6 +53,7 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body':json.dumps(item)
     }
+    
 def uniquecodegen():
     code = ""
     for x in range(5):
