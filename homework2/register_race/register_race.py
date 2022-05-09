e3640b15af8fb8beb44ceb1758f1aa7664e9d3a6
Andrea Trapletti <67322071+AndreaTrapletti@users.noreply.github.com>
2022-05-09 13:03:19 +0200

    Update register_race.py

--- homework2/register_race/register_race.py
+++ homework2/register_race/register_race.py
@@ -1,30 +1,19 @@
+import smtplib
 import json,boto3
 from random import randint
-correttore = 0
+
 
 def lambda_handler(event, context):
-    global correttore
-    sess= boto3.Session(region_name='us-east-1')
-    ddb= sess.client('dynamodb')
-    table = 'TokenTable'
+    uniquecode = uniquecodegen()
     nome_gara = event["queryStringParameters"]["race_name"]
     date = event["queryStringParameters"]["race_date"]
     email = event["queryStringParameters"]["email"]
     date = date.split("/")
-    uniquecode = uniquecodegen()
-    token = nome_gara+'_'+uniquecode
     id = nome_gara[0]+nome_gara[1] + nome_gara[2]+ date[1]+date[2]
-    try:
-        ddb.get_item(TableName=table, Key={
-                'race_id': {
-                  'S': id
-                        }
-                    },
-                    )
-        id = id+str(correttore)
-        correttore = correttore+1
-    except KeyError :
-        print("race id non in conflitto")
+    token = nome_gara+'_'+uniquecode
+    sess= boto3.Session(region_name='us-east-1')
+    ddb= sess.client('dynamodb')
+    table = 'TokenTable'
     item = {
           "race_id": {
                 "S": id
@@ -48,12 +37,26 @@ def lambda_handler(event, context):
         "race_id" : id,
         "secret_token" : token
     }
+    
+    sender = "orienteering.infopoint@gmail.com"
+    receiver = email
+    password = "pass_tg5"
+    subject = "Subject: Credenziali accesso gara\n\n"
+    body = "Ecco le sue credenziali: \n\n" + str(item) +"\n\n" + "Grazie e arrivederci"
+    message = subject + body
+    host = "smtp.gmail.com"
+    server = smtplib.SMTP(host, 587)
+    print("ciao")
+    server.ehlo()
+    server.starttls()
+    server.login(sender,password)
+    server.sendmail(sender,receiver, message)
+    
     return {
         
         'statusCode': 200,
         'body':json.dumps(item)
     }
-    
 def uniquecodegen():
     code = ""
     for x in range(5):
